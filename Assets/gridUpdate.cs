63fbd76026f4bd304c456c462fc5516756df842a
AgSimGit <agsimgit@gmail.com>
2018-12-06 18:56:12 -0800

    initial commit

---
+++ Assets/gridUpdate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridUpdate : MonoBehaviour {
+
+public int fert_percent;
+	// Use this for initialization
+	void Start () {
+		  fert_percent = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	        private void OnTriggerEnter(Collider other)
+        {
+			this.fert_percent += 1;
+            Debug.Log("fert_percent is:" + this.fert_percent);
+        }
+
+			private void OnTriggerStay(Collider other)
+			{
+				this.fert_percent +=1; 
+			}
+}
